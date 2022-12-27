a3cd8d018353a698b94d7417286890f1cfdb9827
hans <hans.habraken@nike.com>
2022-12-27 19:53:14 +0100

    day12: added solution

--- day12/main.py
+++ day12/main.py
@@ -3,6 +3,7 @@
 # example -> [[[0,0], [0,1]], [[0,0], [1,0]]]
 
 import copy
+import heapq
 import string
 
 ALL_LEVELS = string.ascii_lowercase
@@ -35,7 +36,7 @@ def find_start_and_end_index(grid):
     return start_index, end_index
 
 
-def find_next_positions(current_position, previous_path, grid):
+def find_next_positions(current_position, grid):
     """Calculate all next positions and verify if these are able to go to"""
     current_level = grid[current_position[0]][current_position[1]]
     if current_level == "S":
@@ -50,22 +51,21 @@ def find_next_positions(current_position, previous_path, grid):
         [current_position[0], current_position[1] - 1],  # left
         [current_position[0], current_position[1] + 1],  # right
     ]:
-        # Prevent loops
-        if next_position not in previous_path:
-            if (
-                next_position[0] >= 0  # checks top border
-                and next_position[1] >= 0  # checks left border
-                and next_position[0] < len(grid)  # checks bottom border
-                and next_position[1] < len(grid[0])  # checks right border
-            ):  # Position needs to b in grid
-                next_level = grid[next_position[0]][next_position[1]]
-                if next_level == "S":
-                    next_level = "a"
-                if next_level == "E":
-                    next_level = "z"
-                if ALL_LEVELS.index(next_level) - ALL_LEVELS.index(current_level) < 2:
-                    # possible to move to this position
-                    possible_positions.append(next_position)
+
+        if (
+            next_position[0] >= 0  # checks top border
+            and next_position[1] >= 0  # checks left border
+            and next_position[0] < len(grid)  # checks bottom border
+            and next_position[1] < len(grid[0])  # checks right border
+        ):  # Position needs to b in grid
+            next_level = grid[next_position[0]][next_position[1]]
+            if next_level == "S":
+                next_level = "a"
+            if next_level == "E":
+                next_level = "z"
+            if ALL_LEVELS.index(next_level) - ALL_LEVELS.index(current_level) < 2:
+                # possible to move to this position
+                possible_positions.append(next_position)
 
     return possible_positions
 
@@ -73,8 +73,41 @@ def find_next_positions(current_position, previous_path, grid):
 def main(input_file):
     grid = process_input_file(input_file)
     start_position, end_position = find_start_and_end_index(grid)
+
     paths = [[start_position]]
     visited_positions = []
+    found = False
+
+    while not found:
+        heapq.heapify(paths)
+        path = heapq.nsmallest(1, paths, key=len)[0]
+        paths.remove(path)
+        # path = heapq.heappop(paths)
+        last_position = path[-1]
+        next_possible_positions = find_next_positions(
+            current_position=last_position, grid=grid
+        )
+
+        for next_possible_position in next_possible_positions:
+            # print(next_possible_position)
+            if next_possible_position not in visited_positions:
+                next_path = path + [next_possible_position]
+
+                heapq.heappush(paths, next_path)
+                visited_positions.append(next_possible_position)
+
+                if next_possible_position == end_position:
+                    print(len(next_path) - 1)
+                    found = True
+                    break
+
+        if found:
+            break
+
+    # print(paths)
+    # shortest_path = heapq.nsmallest(1, paths, key=len)[0]
+    # print(shortest_path)
+    # print(len(shortest_path) - 1)
 
 
 #     found = False
